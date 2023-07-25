b457b652e01a0b8b47b30a9da2935841eaf36ba4
Roque26 <roquejrfuentes2610@gmail.com>
2023-07-25 13:59:22 +0200

    p

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function sumar (x,y){
+let resultado = x + y 
+
+    return resultado
+}
+
+function restar (x,y){
+    let resultado = x - y 
+    
+        return resultado
+    }
+    
+ function product (x,y){
+ let resultado = x * y 
+        
+     return resultado
+ }
+
+function dividir (x,y){
+ let resultado = x / y 
+            
+return resultado
+
+}
+function cuadrado(x){
+    let cuadrado = x^2
+    return cuadrado
+}
+
+ module.exports= {sumar,restar,product,dividir,cuadrado}
+            
---
+++ run.js
@@ -0,0 +1,13 @@
+
+let myLib = require("./index.js");
+
+
+console.log(myLib.sumar(3,5));
+
+console.log(myLib.restar(3,5));
+
+console.log(myLib.product(3,5));
+
+console.log(myLib.dividir(3,5));
+
+console.log(myLib.cuadrado(2)); 
